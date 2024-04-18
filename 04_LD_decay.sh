a0c5684a7b59e01e711ce1e9765766c7ef74c50d
liujiaxin7 <61490752+liujiaxin7@users.noreply.github.com>
2024-04-18 10:33:05 +0800

    Update 04_LD_decay.sh

--- 04_LD_decay.sh
+++ 04_LD_decay.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 ## step1:
-cut -f 1 clean.ped | sort | uniq | while read i; do grep "^$i[[:space:]]" clean.ped > ${i}.ped; cp clean.map ${i}.map; done
+cut -f 1 clean.ped | sort | uniq | while read i
+do
+        grep "^$i[[:space:]]" clean.ped > ${i}.ped; cp clean.map ${i}.map
+done
 
 ## step2:
-cut -f 1 clean.ped | sort | uniq | while read i; do plink  --file ${i} --sheep --recode vcf-iid --out ${i}; done
+cut -f 1 clean.ped | sort | uniq | while read i
+do
+        plink  --file ${i} --sheep --recode vcf-iid --out ${i}
+done
 
 ## step3:
-cut -f 1 clean.ped | sort | uniq | while read i; do PopLDdecay  -InVCF  ${i}.vcf  -OutStat ${i}_lddecay ; done
+cut -f 1 clean.ped | sort | uniq | while read i
+do
+        PopLDdecay  -InVCF  ${i}.vcf  -OutStat ${i}_lddecay
+done
 
 ## step4:
 find *.gz | awk -F "_" '{OFS = "\t"; print $0, $1}' > pop.list
